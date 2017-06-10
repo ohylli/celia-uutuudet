992b5e7845abb04e244a30da71812fbc60d06cee
Otto Hylli <otto.hylli@tut.fi>
2017-06-10 19:31:26 +0300

    Erotettu luettelon html:n käsittely omaksi metodiksi
    luettelon html käsitellään nyt käsitteleSivu generaattori metodissa, joka
    generoi joko otsikoita luetteloon tai kirja sivujen urleja
    Tarkoitus on että tämä metodi voidaan ylikirjoittaa erilaisten luetteloiden
    käsittelyä varten tehdyissä alaluokissa..

--- uutuushaku.py
+++ uutuushaku.py
@@ -5,7 +5,7 @@ import requests
 # kirjasto HTML:n parsimiseen
 from bs4 import BeautifulSoup
 
-class uutuusHaku():
+class UutuusHaku():
     
     def __init__( self, luetteloUrl, tiedostonimi ):
         # uutuudet sivun osoite
@@ -30,30 +30,16 @@ class uutuusHaku():
         
     def haeKirjat( self ):
         # haetaan ja parsitaan uutuudet sivun html
-        uutuudetSivu = self.haeSivu( self.url )
-
-        # käydään sivun sisältö läpi elementti kerrallaan
-        # lähtien ensimmäisestä kolmos tason ) otsikosta (h3, joka on sivun pääsisältö alueella
-        # tarkemmin käsitellään ensimmäistä h3:a edeltävää elementtiä seuraavat saman tason elementit
-        # näin ensimmäinenkin h3 tulee mukaan
-        for elementti in uutuudetSivu.find( role='main' ).h3.previous_sibling.next_siblings:
-            if elementti.name == 'h3':
-                # elementti on otsikko, jossa on uutuuskategorian nimi kuten jännityskirjallisuus tai pistekirjat
-                # otetaan otsikon teksti ja kirjoitetaan tiedostoon
-                kategoria = elementti.get_text() 
-                self.kirjoita( kategoria )
+        self.uutuudetSivu = self.haeSivu( self.url )
+        for elementti in self.käsitteleSivu():
+            if elementti['tyyppi'] == 'kategoria':
+                self.kirjoita( elementti['kategoria'] )
         
-            elif elementti.name == 'p':
-                # elementti on tekstikappale, jossa kirjan nimi, tekijä ja tunnus
-                # otetaan nämä tiedot talteen ja kirjoitetaan tiedostoon
-                kirja = elementti.get_text()
-                self.kirjoita( kirja )
-                # kappaleessa on myös linkki kirjan kuvauksen sisältävälle sivulle
-                # otetaan osoite talteen
-                kirjaURL = elementti.a['href']
+            elif elementti['tyyppi'] == 'linkki':
+                self.kirjoita( elementti['kirja'] )
                 # ja haetaan kirjan tiedot sisältävä sivu
                 # parsitaan kirjan kuvauksen sisältävän sivun html
-                kirjaSivu = self.haeSivu( kirjaURL )
+                kirjaSivu = self.haeSivu( elementti['kirjaURL'] )
                 # kuvaus on sivun ensimmäisessä tekstikappaleessa
                 # otetaan talteen ja kirjoitetaan tiedostoon
                 kuvaus = kirjaSivu.p.get_text()
@@ -62,6 +48,28 @@ class uutuusHaku():
         # valmista tuli, suljetaan tiedosto
         self.tiedosto.close()
         
+    def käsitteleSivu( self ):
+        # käydään sivun sisältö läpi elementti kerrallaan
+        # lähtien ensimmäisestä kolmos tason ) otsikosta (h3, joka on sivun pääsisältö alueella
+        # tarkemmin käsitellään ensimmäistä h3:a edeltävää elementtiä seuraavat saman tason elementit
+        # näin ensimmäinenkin h3 tulee mukaan
+        for elementti in self.uutuudetSivu.find( role='main' ).h3.previous_sibling.next_siblings:
+            if elementti.name == 'h3':
+                # elementti on otsikko, jossa on uutuuskategorian nimi kuten jännityskirjallisuus tai pistekirjat
+                # otetaan otsikon teksti ja kirjoitetaan tiedostoon
+                kategoria = elementti.get_text() 
+                yield { 'tyyppi': 'kategoria', 'kategoria': kategoria }
+                
+            elif elementti.name == 'p':
+                tulos = { 'tyyppi': 'linkki' }
+                # elementti on tekstikappale, jossa kirjan nimi, tekijä ja tunnus
+                # otetaan nämä tiedot talteen 
+                tulos['kirja'] = elementti.get_text()
+                # kappaleessa on myös linkki kirjan kuvauksen sisältävälle sivulle
+                # otetaan osoite talteen
+                tulos['kirjaURL'] = elementti.a['href']
+                yield tulos
+                        
 if __name__ == '__main__':
-    haku = uutuusHaku( 'https://www.celianet.fi/kirjavinkit/uutuuskirjat-aikuisille/', 'uutuudet.txt' )
+    haku = UutuusHaku( 'https://www.celianet.fi/kirjavinkit/uutuuskirjat-aikuisille/', 'uutuudet.txt' )
     haku.haeKirjat()
