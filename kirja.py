a4894fe90e5c1cd8263f33807590ad52a92370c8
Otto Hylli <otto.hylli@gmail.com>
2017-06-17 15:54:22 +0300

    Lisätty kommentteja

--- kirja.py
+++ kirja.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 class KirjaSivu():
-    """docstring for KirjaSivu"""
-    
+    """Luokka yksittäisen kirjan tiedot sisältävän sivun käsittelyyn."""
+
+        # tekstipohja luetteloon kirjoitettaville kirjan tiedoille: nimi, tekijä, id, tyyppikohtaiset tiedot, url ja kuvaus
+        # pohjaa käytetään pythonin merkkijonon format metodin kanssa
     pohja = '''{nimi} / {tekijä} {id} {tyyppiTiedot}
 {url}
 
 {kuvaus}'''
 
+    # pohjat erityyppisten kirjojen (äänikirja, pistekirja, elektroninen kirja) erityis tiedoille
+    # esim. äänikirjalla on lukija ja kesto, pistekirjalla vihkojen määrä
     tyyppiPohjat = {
         'äänikirja': '''
 lukija: {lukija}, kesto: {kesto}''',
@@ -17,44 +21,64 @@ lukija: {lukija}, kesto: {kesto}''',
     }
     
     def __init__( self, sivu, url ):
+        """Luo KirjaSivu sivun valmiiksi parsitusta sisällöstä (BeautifulSoup olio) ja sivun osoitteesta."""
         self.sivu = sivu
         self.url = url
+        # sivulla on taulukko, jossa on osa kirjan tiedoista
         self.tiedotTaulukko = self.sivu.table
         
     def käsitteleKirjaSivu( self ):
+        """Käsittelee kirjan sivun ja palauttaa kirjan tiedot sisältävän merkkijonon, jonka voi kirjoittaa luetteloon."""
         # tallennetaan kirjan tiedot sanakirjaan
         kirja = { 'url': self.url }
+        # kirjan id löytyy osoitteen lopusta polun viimeisenä kohtana
         kirja['id'] = self.url.split( '/' )[-2]
         # kirjan nimi on ensimmäisessä ykköstason otsikossa
         kirja['nimi'] = self.sivu.h1.string.strip()
         # kuvaus on sivun ensimmäisessä tekstikappaleessa
         kirja['kuvaus'] = self.sivu.p.get_text()
+        # tekijän nimi löytyy sivun taulukosta
         kirja['tekijä'] = self.haeTaulukosta( 'Tekijä', '' )
+        # taulukon ulkoasu kohdassa mainitaan kirjan tyyppi
         ulkoasu = self.haeTaulukosta( 'Ulkoasu' )
+        # tyyppi saadaan ulkoasun ensimmäisestä sanasta, loput ovat tyyppikohtaista lisätietoa
         tyyppi, tyyppiTiedot = ulkoasu.split( maxsplit = 1 )
         
+        # haetaan kirjan tyyppikohtaiset tiedot
         if tyyppi in [ 'äänikirja', 'DaisyTrio', 'talbok' ]:
+            # kirja on äänikirja
             tyyppi = 'äänikirja'
+            # lukija ja kesto löytyvät taulukosta, mutta saattavat puuttua
             kirja['lukija'] = self.haeTaulukosta( 'Lukija', 'ei tiedossa' )
             kirja['kesto'] = self.haeTaulukosta( 'Kesto', 'ei tiedossa' )
             
         elif tyyppi == 'pistekirja':
+            # pistekirjan tiedot ovat ulkoasu kohdan lopussa
+            # ne ovat sulkumerkkien sisällä, jotka tässä poistetaan
             kirja['pisteTiedot'] = tyyppiTiedot[1:-1]
             
         elif tyyppi == 'elektroninen':
+            # elektroniseen kirjaan liittyen ei haeta tyyppitietoja
             pass
             
         else:
             print( 'Tuntematon kirjan tyyppi ' +tyyppi )
             quit()
-            
+        
+        # tehdään tyyppikohtaisista tiedoista luetteloon kirjoitettava versio
+        # käytettävä pohja riippuu kirjan tyypistä
         kirja['tyyppiTiedot'] = self.tyyppiPohjat[tyyppi].format( **kirja )
+        # tehdään kirjan tiedoista luetteloon kirjoitettava merkkijono pohjasta
         return self.pohja.format( **kirja )
         
     def haeTaulukosta( self, tieto, oletus = None ):
+        """Hakee halutun tiedon kirjasivulla olevasta taulukosta. Palauttaa annetun oletuksen, jos taulukossa ei ole kysyttyä tietoa."""
+        # haetaan taulukon solu, jossa on kysytty teksti
         td = self.tiedotTaulukko.find( 'td', text = tieto )
         if td != None:
+            # tieto löytyi palautetaan se eli tekstin viereisen taulukon solun sisältö
             return td.next_sibling.string
         
         else:
+            # tietoa ei löydy taulukosta
             return oletus
--- uutuusluettelo.py
+++ uutuusluettelo.py
@@ -1,74 +1,110 @@
 ﻿# -*- coding: utf-8 -*-
+# sisältää luokat uutuusluettelosivun käsittelyyn
 
 # kirjasto HTTP pyyntöjen tekemiseen eli nettisivun hakuun
 import requests
 # kirjasto HTML:n parsimiseen
 from bs4 import BeautifulSoup
 
+# luokka yksittäisen kirjan tiedot sisältävän sivun käsittelyyn
 from kirja import KirjaSivu
 
 class Uutuusluettelo():
+    """Luokka uutuusluettelosivun käsittelyyn
+    
+    Luokka käy uutuudet sivun sisällön läpi luoden siitä tekstimuotoisen uutuusluettelon."""
     
     def __init__( self, luetteloUrl, tiedostonimi ):
+        """Luo Utuusluettelo käsittelemään annettu uutuudet sivu, jonka sisältö tallennetaan annetun nimiseen tiedostoon."""
         # uutuudet sivun osoite
         self.url = luetteloUrl
         # avataan tiedosto tietojen tallentamista varten
         self.tiedosto = open( tiedostonimi, 'wb' )
 
-    # funktio joka kirjoittaa annetun sisällön annettuun tiedostoon
     def kirjoita( self, sisältö ):
+        """Kirjoittaa annetun sisällön luettelotiedostoon."""
         # lisätään pari rivinvaihtoa sisällön perään ja tallennetaan utf-8 koodauksella
         self.tiedosto.write( bytes( sisältö +'\r\n\r\n', 'utf-8' ))
 
     def haeSivu( self, url ):
+        """Hakee annetussa osoitteessa olevan sivun.
+        
+        Palauttaa valmiiksi käsitellyn HTML:n BeautifulSoup oliona.
+        Ohjelman suoritus lopetetaan, jos sivua ei saada haettua."""
+        # haetaan sivu
         vastaus = requests.get( url )
         # tarkistetaan, että pyyntö onnistui
         if vastaus.status_code != 200:
-            print( 'Sivun haku epäonnistui. HTTP status koodi: ' +str( vastaus.status_code ))
+            print( 'Sivun ' +url +'  haku epäonnistui. HTTP status koodi: ' +str( vastaus.status_code ))
             # lopetetaan ohjelma
             quit()
-            
+
+         # parsitaan sivun HTML
         return BeautifulSoup( vastaus.text, 'html.parser' )
         
     def haeKirjat( self ):
+        """Hakee uutuussivun kirjat ja tallentaa niiden tiedot tekstitiedostoon."""
         # haetaan ja parsitaan uutuudet sivun html
         self.uutuudetSivu = self.haeSivu( self.url )
+        # käsitellään sivun sisältö
+        # käsitteleLuettelo generaattori palauttaa joko sivun eri kirja kategorioiden otsikoita
+        # tai yksittäisen kirjan tiedot sisältävän sivun osoitteen
         for elementti in self.käsitteleLuetteloSivu():
             if elementti['tyyppi'] == 'kategoria':
+                # elementti on kirjakategorian otsikko, joka kirjoitetaan luettelotiedostoon
                 self.kirjoita( elementti['kategoria'] )
         
             elif elementti['tyyppi'] == 'linkki':
+                # elementti on linkki yksittäisen kirjan tiedot sivulle
                 # haetaan kirjan tiedot sisältävä sivu
-                # parsitaan kirjan kuvauksen sisältävän sivun html
                 kirjaSivu = self.haeSivu( elementti['kirjaURL'] )
+                # luodaan KirjaSivu olio käsittelemään kirjan tiedot
+                # haetaan KirjaSivulta kirjan tiedot tekstimuodossa
                 kirjaTiedot = KirjaSivu( kirjaSivu, elementti['kirjaURL'] ).käsitteleKirjaSivu()
+                # tallennetaan tiedot tiedostoon
                 self.kirjoita( kirjaTiedot )
     
         # valmista tuli, suljetaan tiedosto
         self.tiedosto.close()
         
     def käsitteleLuetteloSivu( self ):
+        """Generaattori funktio uutuudet sivun läpikäymiseen.
+        
+        Metodi generoi sivun sisällöstä sanakirjoja, joissa on elementin tiedot.
+        Elementit ovat joko sivun eri kirja kategorioiden nimiä tai linkkejä kirjan tiedot sisältävälle sivulle.
+        Sanakirjan tyyppi attribuutti kertoo tyypin, joka on joko kategoria tai linkki.
+        Kategoria tyypin elementillä on lisäksi kategoria attribuuutti ja linkki tyypillä linkki."""
         # käydään sivun sisältö läpi elementti kerrallaan
-        # lähtien ensimmäisestä kolmos tason ) otsikosta (h3, joka on sivun pääsisältö alueella
-        # tarkemmin käsitellään ensimmäistä h3:a edeltävää elementtiä seuraavat saman tason elementit
-        # näin ensimmäinenkin h3 tulee mukaan
+        # lähtien ensimmäisestä kiinnostavasta elementistä, joka haetaan omalla metodillaan
+        # käsitellään tätä elementtiä seuraavat saman tason elementit, jotka ovat joko otsikoita tai tekstikappaleita
         for elementti in self.ekaElementti().next_siblings:
             if elementti.name == 'h3':
                 # elementti on otsikko, jossa on uutuuskategorian nimi kuten jännityskirjallisuus tai pistekirjat
-                # otetaan otsikon teksti ja kirjoitetaan tiedostoon
+                # otetaan otsikon teksti 
                 kategoria = elementti.get_text() 
+                # generoidaan elementti kategoriasta
                 yield { 'tyyppi': 'kategoria', 'kategoria': kategoria }
                 
             elif elementti.name == 'p':
+                # tekstikappale, jossa on yksi tai useampi linkki kirja sivulle
                 for linkki in elementti.find_all( 'a' ):
+                    # tehdään linkistä elementti
                     tulos = { 'tyyppi': 'linkki' }
+                    # otetaan linkin osoite talteen
                     tulos['kirjaURL'] = linkki['href']
                     yield tulos
                     
     def ekaElementti( self ):
+        """Metodi ensimmäisen sivun kiinnostavan elementin hakemiseen."""
+        # haetaan sivun pääosion ensimmäistä kolmos tason otsikkoa edeltävä elementti
         return self.uutuudetSivu.find( role='main' ).h3.previous_sibling
         
 class NuortenUutuudet( Uutuusluettelo ):
+    """Uutuusluettelon aliluokka nuorten uutuudetsivun käsittelyyn
+    
+    Sivun rakenne on hieman erilainen tarkemmin sen pääosiossa luettelo alkaa toisesta kolmostason otsikosta ei ensimmäisestä."""
 
     def ekaElementti( self ):
+        """Metodi ensimmäisen käsiteltävän elementin hakuun."""
+        # haetaan sivun pääosion toista kolmostason otsikkoa edeltävä elementti
         return self.uutuudetSivu.find( role='main' ).find_all( 'h3', limit = 2 )[1].previous_sibling
