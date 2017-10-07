260c05297c0a042cbc30a91252c2cdfa89a7b05b
Otto Hylli <otto.hylli@gmail.com>
2017-10-07 20:37:15 +0300

    Paranna postittajan virheenkäsittelyä
    Käsitellään puuttuva sekä virheellinen konfiguraatiotiedosto
    ja yhdistämisongelmat postipalvelimen kanssa.

--- posti.py
+++ posti.py
@@ -3,6 +3,10 @@
 from email.mime.text import MIMEText
 import smtplib
 import json
+import socket
+
+# konfiguraatio tiedoston nimi
+KONFIGURAATIO_TIEDOSTO = 'posti.json'
 
 # Luettelon alkuun liitettävä teksti
 alku = '''Tämä luettelo on luotu ja lähetetty automaattisesti, joten se saattaa sisältää virheitä. Luettelon luomiseen käytetyn ohjelman lähdekoodi on saatavissa osoitteessa:
@@ -16,40 +20,72 @@ class Postittaja():
     def __init__( self ):
         '''Luo Postittaja posti.json konfiguraatio tiedoston määräämillä asetuksilla.'''
         # lue konfiguraatio tiedostosta ja muunna json python sanakirjaksi
-        with open( 'posti.json', 'r', encoding = 'utf-8' ) as tiedosto:
-            konfiguraatio = json.load( tiedosto )
+        try:
+            with open( KONFIGURAATIO_TIEDOSTO, 'r', encoding = 'utf-8' ) as tiedosto:
+                konfiguraatio = json.load( tiedosto )
+        
+        except FileNotFoundError:
+            print( 'Postittajan konfiguraatio tiedostoa {} ei löytynyt.'.format( KONFIGURAATIO_TIEDOSTO ))
+            quit()
             
-        # tallenna konfiguraation sisältö muuttujiin
-        self.lähettäjä = konfiguraatio[ 'lähettäjä' ]
-        self.vastaanottajat = konfiguraatio[ 'vastaanottajat' ]
-        if 'piilo_vastaanottajat' in konfiguraatio:
-            self.piiloVastaanottajat = konfiguraatio[ 'piilo_vastaanottajat' ]
+        except json.decoder.JSONDecodeError as virhe:
+            print( 'Virhe postittajan konfiguraatio tiedoston käsittelyssä: ' +str( virhe ))
+            quit()
+        
+        try:
+            # tallenna konfiguraation sisältö muuttujiin
+            self.lähettäjä = konfiguraatio[ 'lähettäjä' ]
+            self.vastaanottajat = konfiguraatio[ 'vastaanottajat' ]
+            if 'piilo_vastaanottajat' in konfiguraatio:
+                self.piiloVastaanottajat = konfiguraatio[ 'piilo_vastaanottajat' ]
             
-        else:
-            self.piiloVastaanottajat = None
+            else:
+                self.piiloVastaanottajat = None
             
-        palvelinNimi = konfiguraatio['palvelin']
-        portti = konfiguraatio[ 'portti' ]
-        käyttäjä = konfiguraatio[ 'käyttäjä' ]
-        salasana = konfiguraatio[ 'salasana' ]
+            palvelinNimi = konfiguraatio['palvelin']
+            portti = konfiguraatio[ 'portti' ]
+            käyttäjä = konfiguraatio[ 'käyttäjä' ]
+            salasana = konfiguraatio[ 'salasana' ]
+            
+        except KeyError as virhe:
+            print( '{} puuttuu postittajan konfiguraatiotiedostosta.'.format( virhe.args[0] ))
+            quit()
         
         # Luo asiakas sähköpostipalvelimelle, joka on määritelty konfiguraatiotiedostossa
         # käytetään heti SSL salattua yhteyttä
         # ainakin soneran palvelin mail.inet.fi vaatii heti salatun yhteyden eli ei voida luoda salaamatonta asiakasta ja sitten käynnistää salattua yhteyttä
-        self.palvelin = smtplib.SMTP_SSL( palvelinNimi, portti )
-        # kirjaudutaan palvelimelle konfiguraatiossa olleilla käyttäjätunnuksella ja salasanalla
-        self.palvelin.login( käyttäjä, salasana )
+        try:
+            self.palvelin = smtplib.SMTP_SSL( palvelinNimi, portti )
+            # kirjaudutaan palvelimelle konfiguraatiossa olleilla käyttäjätunnuksella ja salasanalla
+            self.palvelin.login( käyttäjä, salasana )
+            
+        except ConnectionRefusedError:
+            print( 'Posti palvelin kieltäytyi yhteydestä. Portti voi olla väärä.' )
+            quit()
+            
+        except socket.gaierror:
+            print( 'Yhteys postipalvelimeen epäonnistui. Tarkista, että palvelimen osoite on oikea.' )
+            quit()
+            
+        except smtplib.SMTPAuthenticationError:
+            print( 'Kirjautuminen postipalvelimelle ei onnistunut. Tarkista käyttäjätunnus ja salasana.' )
+            quit()
         
     def postita( self, luettelo ):
         '''Postittaa parametrina saadun luettelo sanakirjan määrittämän uutuusluettelon.'''
         # luetaan uutuusluettelon sisältö tiedostosta
-        tiedosto = open( luettelo['tiedosto'], 'r' )
-        runko = tiedosto.read()
+        try:
+            with open( luettelo['tiedosto'], 'r' ) as tiedosto:
+                runko = tiedosto.read()
+                
+        except FileNotFoundError:
+                print( 'Postittaja ei löytänyt luettelotiedostoa {}.'.format( luettelo['tiedosto'] ))
+                return
+                
         if len( runko ) == 0:
             print( 'Luettelo {} on tyhjä eikä sitä postiteta.'.format( luettelo['tiedosto'] ))
             return
             
-        tiedosto.close()
         # luodaan tekstimuotoinen lähetettävä viesti, jonka rungoksi asetetaan luettelon sisältö
         viesti = MIMEText( alku +runko, 'plain' )
         # asetetaan viestin lähettäjä
